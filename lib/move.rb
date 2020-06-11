1ae7ae4328c80a90a66dd2a800a32d4fcb7c2ac6
Nick Spangler <spangleyman@mac.com>
2020-06-11 17:42:20 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, index, character = "X")
-  board[index] = character
+def move(board, index, token = "X")
+  board[index] = token
 end
